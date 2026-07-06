30439a540b4e6d000af0eca516cb01422043bc35
ypolash <yapbit3@gmail.com>
2026-07-06 18:34:08 +0600

    Auto-save: Added Regenerative Medicine banner image to Featured Products section on homepage

--- source_code/core/resources/views/front/themes/theme1.blade.php
+++ source_code/core/resources/views/front/themes/theme1.blade.php
@@ -376,6 +376,16 @@
                         </div>
                     </div>
                 </div>
+                <div class="row mb-30">
+                    <div class="col-lg-12">
+                        <a href="{{ route('front.catalog') }}" class="d-block">
+                            <img src="{{ asset('assets/images/featured-banner.png') }}"
+                                 alt="Regenerative Medicine - All About The Procedure And Treatment Options"
+                                 class="img-fluid w-100"
+                                 style="border-radius: 8px; max-height: 320px; object-fit: cover;">
+                        </a>
+                    </div>
+                </div>
                 <div class="row">
                     <div class="type_product_view d-none">
                         <img  src="{{asset('assets/images/ajax_loader.gif')}}" alt="">
