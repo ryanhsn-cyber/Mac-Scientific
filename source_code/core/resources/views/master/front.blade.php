763bdcae9f31e249bc080bbc2a1365ad07a0b6b9
ypolash <yapbit3@gmail.com>
2026-07-20 12:11:37 +0600

    Auto-save: Async load responsive.css and paymentfont.min.css

--- source_code/core/resources/views/master/front.blade.php
+++ source_code/core/resources/views/master/front.blade.php
@@ -39,9 +39,13 @@
 @yield('styleplugins')
 
 <link id="mainStyles" rel="stylesheet" media="screen" href="{{asset('assets/front/css/styles.min.css')}}">
-<link id="responsiveStyles" rel="stylesheet" media="screen" href="{{asset('assets/front/css/responsive.css')}}">
-<link rel="preload" href="{{asset('assets/front/css/paymentfont.min.css')}}" as="style">
-<link rel="stylesheet" media="screen" href="{{asset('assets/front/css/paymentfont.min.css')}}">
+
+<!-- Async Non-Critical CSS -->
+<link id="responsiveStyles" rel="stylesheet" media="print" onload="this.media='all'" href="{{asset('assets/front/css/responsive.css')}}">
+<noscript><link rel="stylesheet" href="{{asset('assets/front/css/responsive.css')}}"></noscript>
+
+<link rel="stylesheet" media="print" onload="this.media='all'" href="{{asset('assets/front/css/paymentfont.min.css')}}">
+<noscript><link rel="stylesheet" href="{{asset('assets/front/css/paymentfont.min.css')}}"></noscript>
 <!-- Color css -->
 <style>
 @php
